25f1f8cb9006c1a1e94287c60210ccd51e1135ea
KadamQ <kovadam95@gmail.com>
2019-11-24 02:44:47 +0100

    Add about component to the website

--- DentalWebsiteProject/DentalWebsite/src/app/app.module.ts
+++ DentalWebsiteProject/DentalWebsite/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
