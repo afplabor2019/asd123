3d91cb10429ffee503f1c7e12ddc395ab925467e
KadamQ <kovadam95@gmail.com>
2019-11-24 02:24:20 +0100

    Add home component to the website

--- DentalWebsiteProject/DentalWebsite/src/app/app.module.ts
+++ DentalWebsiteProject/DentalWebsite/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
